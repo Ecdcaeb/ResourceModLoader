602ba030b9f36f6b5840c34236f3e1d28e3e6266
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2024-02-07 08:31:02 +0800

    support all type that forgeConfig support

--- src/main/java/mods/Hileb/rml/api/clazz/ClassValueTransformer.java
+++ src/main/java/mods/Hileb/rml/api/clazz/ClassValueTransformer.java
@@ -4,12 +4,12 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import mods.Hileb.rml.api.EarlyClass;
 import mods.Hileb.rml.api.PublicAPI;
-import net.minecraftforge.common.config.TypeAdapters;
+import mods.Hileb.rml.api.file.JsonHelper;
+import mods.Hileb.rml.core.RMLFMLLoadingPlugin;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 
 /**
@@ -29,55 +29,49 @@ public class ClassValueTransformer {
                 transform(field.getType(),jsonElement.getAsJsonObject(),field.get(instance));
             }else {
                 Class<?> type = field.getType();
-                if (int.class == type) field.set(instance,jsonElement.getAsInt());
-                else if (float.class == type) field.set(instance,jsonElement.getAsFloat());
-                else if (boolean.class == type) field.set(instance,jsonElement.getAsBoolean());
-                else if (byte.class == type) field.set(instance,jsonElement.getAsByte());
-                else if (char.class == type) field.set(instance,jsonElement.getAsCharacter());
-                else if (double.class == type) field.set(instance,jsonElement.getAsDouble());
-                else if (long.class == type) field.set(instance,jsonElement.getAsLong());
-                else if (String.class == type) field.set(instance,jsonElement.getAsString());
-                else if (int[].class ==type) field.set(instance, G);
+                JsonHelper.JsonReader jsonReader = cachedBaseType.get(type);
+                if (jsonReader !=null){
+                    field.set(instance,jsonReader.read(jsonElement));
+                }else {
+                    RMLFMLLoadingPlugin.Container.LOGGER.error(new UnsupportedOperationException("the json be not supported. Error at "+ jsonElement));
+                }
             }
         }
     }
-    private static final HashMap<Class<?>,JsonReader<?>> cachedBaseType=new HashMap<>();
+    public static final HashMap<Class<?>, JsonHelper.JsonReader<?>> cachedBaseType=new HashMap<>();
     static {
-        cachedBaseType.put(boolean.class);
-        cachedBaseType.put(boolean[].class);
-        cachedBaseType.put(Boolean.class);
-        cachedBaseType.put(Boolean[].class);
-        cachedBaseType.put(float.class;
-        cachedBaseType.put(float[].class);
-        cachedBaseType.put(Float.class);
-        cachedBaseType.put(Float[].class);
-        cachedBaseType.put(double.class);
-        cachedBaseType.put(double[].class);
-        cachedBaseType.put(Double.class);
-        cachedBaseType.put(Double[].class);
-        cachedBaseType.put(byte.class);
-        cachedBaseType.put(byte[].class);
-        cachedBaseType.put(Byte.class);
-        cachedBaseType.put(Byte[].class);
-        cachedBaseType.put(char.class);
-        cachedBaseType.put(char[].class);
-        cachedBaseType.put(Character.class);
-        cachedBaseType.put(Character[].class);
-        cachedBaseType.put(short.class);
-        cachedBaseType.put(short[].class);
-        cachedBaseType.put(Short.class);
-        cachedBaseType.put(Short[].class);
-        cachedBaseType.put(int.class);
-        cachedBaseType.put(int[].class);
-        cachedBaseType.put(Integer.class);
-        cachedBaseType.put(Integer[].class);
-        cachedBaseType.put(String.class);
-        cachedBaseType.put(String[].class);
+        register(boolean.class, JsonHelper::getBoolean);
+        register(boolean[].class, JsonHelper::getBooleanArray);
+        register(Boolean.class, JsonHelper::getBoolean);
+        register(Boolean[].class, (element)-> JsonHelper.getAsArray(element,JsonHelper::getBoolean));
+        register(float.class, JsonHelper::getFloat);
+        register(float[].class, JsonHelper::getFloatArray);
+        register(Float.class, JsonHelper::getFloat);
+        register(Float[].class, (element)-> JsonHelper.getAsArray(element,JsonHelper::getFloat));
+        register(double.class, JsonHelper::getDouble);
+        register(double[].class, JsonHelper::getDoubleArray);
+        register(Double.class, JsonHelper::getDouble);
+        register(Double[].class, (element)-> JsonHelper.getAsArray(element,JsonHelper::getDouble));
+        register(byte.class, JsonHelper::getByte);
+        register(byte[].class, JsonHelper::getByteArray);
+        register(Byte.class, JsonHelper::getByte);
+        register(Byte[].class, (element)-> JsonHelper.getAsArray(element,JsonHelper::getByte));
+        register(char.class, JsonHelper::getChar);
+        register(char[].class, JsonHelper::getCharArray);
+        register(Character.class, JsonHelper::getChar);
+        register(Character[].class, (element)-> JsonHelper.getAsArray(element,JsonHelper::getChar));
+        register(short.class, JsonHelper::getShort);
+        register(short[].class, JsonHelper::getShortArray);
+        register(Short.class, JsonHelper::getShort);
+        register(Short[].class, (element)-> JsonHelper.getAsArray(element,JsonHelper::getShort));
+        register(int.class, JsonHelper::getInteger);
+        register(int[].class, JsonHelper::getIntegerArray);
+        register(Integer.class, JsonHelper::getInteger);
+        register(Integer[].class, (element)-> JsonHelper.getAsArray(element,JsonHelper::getInteger));
+        register(String.class, JsonHelper::getString);
+        register(String[].class, (element)-> JsonHelper.getAsArray(element,JsonHelper::getString));
     }
-    public static <T> JsonReader<T> getReader(Class<T> clazz){
-
-    }
-    public interface JsonReader<T>{
-        T read(JsonElement jsonElement);
+    public static <T> void register(Class<T> clazz, JsonHelper.JsonReader<T> reader){
+        cachedBaseType.put(clazz,reader);
     }
 }
--- src/main/java/mods/Hileb/rml/api/file/JsonHelper.java
+++ src/main/java/mods/Hileb/rml/api/file/JsonHelper.java
@@ -1,8 +1,11 @@
 package mods.Hileb.rml.api.file;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import mods.Hileb.rml.api.EarlyClass;
 import mods.Hileb.rml.api.PublicAPI;
+
+import java.util.ArrayList;
 
 /**
  * @Project ResourceModLoader
@@ -12,13 +15,109 @@ import mods.Hileb.rml.api.PublicAPI;
 @EarlyClass
 @PublicAPI
 public class JsonHelper {
-    public static boolean getBoolean(JsonElement element){return element.getAsBoolean();}
-    public static int getInteger(JsonElement element){return element.getAsInt();}
-    public static char getChar(JsonElement element){return element.getAsCharacter();}
-    public static String getString(JsonElement element){return element.getAsString();}
-    public static short getShort(JsonElement element){return element.getAsShort();}
-    public static float getFloat(JsonElement element){return element.getAsFloat();}
-    public static boolean getBool(JsonElement element){return element.getAsBoolean();}
-    public static boolean getBool(JsonElement element){return element.getAsBoolean();}
-    public static boolean getBool(JsonElement element){return element.getAsBoolean();}
+    @SuppressWarnings("unchecked")
+    @PublicAPI public static <T> T[] getAsArray(JsonElement element, JsonReader<T> singleReader){
+        JsonArray array=element.getAsJsonArray();
+        int size=array.size();
+        ArrayList<T> arrayList=new ArrayList<>(size);
+        for(int i=0;i<size;i++){
+            arrayList.set(i,singleReader.read(array.get(i)));
+        }
+        return (T[])arrayList.toArray();
+    }
+    @PublicAPI public static boolean getBoolean(JsonElement element){return element.getAsBoolean();}
+    @PublicAPI public static boolean[] getBooleanArray(JsonElement element){
+        JsonArray array=element.getAsJsonArray();
+        int size=array.size();
+        boolean[] result=new boolean[size];
+        for(int i=0;i<size;i++){
+            result[i]=getBoolean(array.get(i));
+        }
+        return result;
+    }
+    @PublicAPI public static int getInteger(JsonElement element){return element.getAsInt();}
+    @PublicAPI public static int[] getIntegerArray(JsonElement element){
+        JsonArray array=element.getAsJsonArray();
+        int size=array.size();
+        int[] result=new int[size];
+        for(int i=0;i<size;i++){
+            result[i]=getInteger(array.get(i));
+        }
+        return result;
+    }
+    @PublicAPI public static char getChar(JsonElement element){return element.getAsCharacter();}
+    @PublicAPI public static char[] getCharArray(JsonElement element){
+        JsonArray array=element.getAsJsonArray();
+        int size=array.size();
+        char[] result=new char[size];
+        for(int i=0;i<size;i++){
+            result[i]=getChar(array.get(i));
+        }
+        return result;
+    }
+    @PublicAPI public static String getString(JsonElement element){return element.getAsString();}
+    @PublicAPI public static String[] getStringArray(JsonElement element){
+        JsonArray array=element.getAsJsonArray();
+        int size=array.size();
+        String[] result=new String[size];
+        for(int i=0;i<size;i++){
+            result[i]=getString(array.get(i));
+        }
+        return result;
+    }
+    @PublicAPI public static short getShort(JsonElement element){return element.getAsShort();}
+    @PublicAPI public static short[] getShortArray(JsonElement element){
+        JsonArray array=element.getAsJsonArray();
+        int size=array.size();
+        short[] result=new short[size];
+        for(int i=0;i<size;i++){
+            result[i]=getShort(array.get(i));
+        }
+        return result;
+    }
+    @PublicAPI public static float getFloat(JsonElement element){return element.getAsFloat();}
+    @PublicAPI  public static float[] getFloatArray(JsonElement element){
+        JsonArray array=element.getAsJsonArray();
+        int size=array.size();
+        float[] result=new float[size];
+        for(int i=0;i<size;i++){
+            result[i]=getFloat(array.get(i));
+        }
+        return result;
+    }
+    @PublicAPI public static byte getByte(JsonElement element){return element.getAsByte();}
+    @PublicAPI public static byte[] getByteArray(JsonElement element){
+        JsonArray array=element.getAsJsonArray();
+        int size=array.size();
+        byte[] result=new byte[size];
+        for(int i=0;i<size;i++){
+            result[i]=getByte(array.get(i));
+        }
+        return result;
+    }
+    @PublicAPI public static double getDouble(JsonElement element){return element.getAsDouble();}
+    @PublicAPI public static double[] getDoubleArray(JsonElement element){
+        JsonArray array=element.getAsJsonArray();
+        int size=array.size();
+        double[] result=new double[size];
+        for(int i=0;i<size;i++){
+            result[i]=getDouble(array.get(i));
+        }
+        return result;
+    }
+    @PublicAPI public static long getLong(JsonElement element){return element.getAsLong();}
+    @PublicAPI public static long[] getLongArray(JsonElement element){
+        JsonArray array=element.getAsJsonArray();
+        int size=array.size();
+        long[] result=new long[size];
+        for(int i=0;i<size;i++){
+            result[i]=getLong(array.get(i));
+        }
+        return result;
+    }
+    @PublicAPI
+    @FunctionalInterface
+    public interface JsonReader<T>{
+        T read(JsonElement jsonElement);
+    }
 }
