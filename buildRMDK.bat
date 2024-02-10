a9b895c79c243f7505dfa800388e2bdb4aeabb95
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2024-02-10 15:40:10 +0800

    Auto Download RMDK.

---
+++ buildRMDK.bat
@@ -0,0 +1,1 @@
+call gradlew.bat buildRMDK --debug --stacktrace --info
--- src/main/java/mods/Hileb/rml/api/config/RunningTimeConfigFactory.java
+++ src/main/java/mods/Hileb/rml/api/config/RunningTimeConfigFactory.java
@@ -1,9 +1,13 @@
 package mods.Hileb.rml.api.config;
 
+import com.google.gson.JsonObject;
+import net.minecraftforge.common.config.Configuration;
+
 /**
  * @Project ResourceModLoader
  * @Author Hileb
  * @Date 2024/2/10 1:00
  **/
 public class RunningTimeConfigFactory { //TODO create a config for rml mod.
+    //public static Configuration makeBase(JsonObject jsonObject){}
 }
