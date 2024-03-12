560b22c22e6ebb47cdf78b903bac756375f7ed7f
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2024-03-12 13:20:46 +0800

    Update ASMHelper.java

--- src/main/java/mods/Hileb/rml/api/asm/ASMHelper.java
+++ src/main/java/mods/Hileb/rml/api/asm/ASMHelper.java
@@ -1,24 +1,5 @@
 package mods.Hileb.rml.api.asm;
 
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.common.MinecraftForge;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.InsnList;
-import org.objectweb.asm.tree.InsnNode;
-import org.objectweb.asm.tree.IntInsnNode;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-
 /**
  * @Project ResourceModLoader
  * @Author Hileb
