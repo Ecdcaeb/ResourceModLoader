a1ca0b3e8720bf951734854c4d02c056cad3848a
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2024-05-25 12:31:11 +0800

    bump version

--- src/main/java/mods/rml/ResourceModLoader.java
+++ src/main/java/mods/rml/ResourceModLoader.java
@@ -25,7 +25,7 @@ import java.util.stream.Collectors;
 @PublicAPI
 public class ResourceModLoader {
     @PublicAPI public static final String MODID = "rml";
-    @PublicAPI public static final String VERSION = "1.0.11";
+    @PublicAPI public static final String VERSION = "1.1.0";
     @PrivateAPI private static final BuffedModIDContainer buffedModIDContainer = new BuffedModIDContainer();
     @PrivateAPI public static final HashSet<ContainerHolder> enabledModContainers = new HashSet<>();
 
